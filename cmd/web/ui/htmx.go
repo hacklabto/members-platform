45048a6612ea28b38c621df63b6fc3270a7b84f1
lillian <lillian@hacklab.to>
2024-01-16 23:01:52 -0500

    fix templates on htmx requests

--- cmd/web/ui/htmx.go
+++ cmd/web/ui/htmx.go
@@ -2,20 +2,29 @@ package ui
 
 import (
 	"log"
+	"members-platform/internal/auth"
 	"net/http"
 )
 
 // todo: integrate this with auth
 // todo: check HX-Current-URL and add title to component-only request
-func MaybeHtmxComponent(rw http.ResponseWriter, r *http.Request, page string, data any) {
+func MaybeHtmxComponent(rw http.ResponseWriter, r *http.Request, page string, pageData any) {
 	if r.Header.Get("HX-Request") == "true" {
+		ctx := r.Context()
+		data := TmplData{
+			Ctx: TmplContext{
+				AuthLevel:       ctx.Value(auth.Ctx__AuthLevel).(auth.AuthLevel),
+				CurrentUsername: ctx.Value(auth.Ctx__AuthenticatedUser).(string),
+			},
+			Data: pageData,
+		}
 		if err := Page(rw, page, data); err != nil {
 			log.Println(err)
 		}
 		return
 	}
 
-	if err := PageWithShell(r.Context(), rw, page, data); err != nil {
+	if err := PageWithShell(r.Context(), rw, page, pageData); err != nil {
 		log.Println(err)
 	}
 }
